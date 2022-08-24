8e44f13bc5a64b7b68187190b096d797aa3ab29c
maesaki22 <volcanor0614@gmail.com>
2022-08-24 22:20:23 +0900

    1-code review

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -12,19 +12,6 @@ public class ArticleForm {
     private String title;
     private String content;
 
-    //public ArticleForm(String title, String content) {
-    //    this.title = title;
-    //   this.content = content;
-    //}
-
-    //@Override
-    //public String toString() {
-    //    return "ArticleForm{" +
-     //           "title='" + title + '\'' +
-     //           ", content='" + content + '\'' +
-    //            '}';
-    //}
-
     public Article toEntity() {
         return new Article(id,title,content);
     }
